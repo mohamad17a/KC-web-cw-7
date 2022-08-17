bc0524c05e563977fc5930a333962c39b751a91c
mohamad almutawaa <shanaba719@gmail.com>
2022-08-17 19:59:47 +0300

    cw7 done its so hard bro

--- script.js
+++ script.js
@@ -1,1 +1,73 @@
+function BMI(weight,heiht) {
+    let result = weight / (height * heiht)
+    return result;
+}
+
+console.log(BMI(70 , 1.7));
+
+function status(bmi) {
+if (bmi < 18.5) {
+    return "لديك نقص في الوزن"
+} else if ( bmi >= 18.5 && bmi <25) {
+    return "وزنك صحي";
+} else {
+    return "لديك زياده قي الوزن";
+}
+
+}
+
+
+function calculate() {
+    let weight = document.getElementById("weight").value;
+    let height = document.getElementById("height").value;
+
+let bmi = BMI(weight, height);
+
+let desc = status(bmi);
+
+let div = document.getElementById("result")
+
+div.innerText = bmi + " == " + desc;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
